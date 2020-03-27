c5e1ed315a04bae64787ddfcdf135de466e0f868
Nandiheath <nandi0315@gmail.com>
2020-03-28 01:14:43 +0800

    initial commit

---
+++ run.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const request = require('request');
+const moment = require('moment');
+const mkdirp = require('mkdirp');
+const path = require('path');
+
+// https://www.chp.gov.hk/files/pdf/building_list_chi.pdf
+// https://www.chp.gov.hk/files/pdf/building_list_eng.pdf
+// https://www.chp.gov.hk/files/pdf/flights_trains_tc.pdf
+// https://www.chp.gov.hk/files/pdf/flights_trains_en.pdf
+
+
+
+const downloadPdf = async (folder, filename, url) => {
+  await new Promise((resolve, reject) => {
+    console.log(folder)
+    mkdirp.sync(folder);
+    const file = fs.createWriteStream(path.join(folder, filename));
+    request({
+      /* Here you should specify the exact link to the file you are trying to download */
+      uri: url,
+      headers: {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+      },
+      /* GZIP true for most of the websites now, disable it if you don't need it */
+      gzip: true
+    })
+      .pipe(file)
+      .on('finish', () => {
+        console.log(`The file is finished downloading.`);
+        resolve();
+      })
+      .on('error', (error) => {
+        reject(error);
+      })
+
+  })
+}
+
+const generateHtml = () => {
+  const folders = fs.readdirSync('data');
+  const paths = [];
+  folders.forEach(dir => {
+    const dateDir = fs.readdirSync('data/' + dir);
+    dateDir.forEach(file => {
+      paths.push({
+        date: dir,
+        file,
+        path: `data/${dir}/${file}`
+      });
+    })
+  })
+
+  const html = `<ul>${paths.map(p => `<li><a href="${p.path}">${p.date}/${p.file}</a></li>`).join('')}</ul>`
+  fs.writeFileSync('index.html', fs.readFileSync('index.html.template').toString().replace('__TEMPLATE__', html));
+}
+
+const run = async () => {
+  const today = moment().format('YYYY-MM-DD');
+  await downloadPdf(`data/${today}`, 'building_list_chi.pdf', 'https://www.chp.gov.hk/files/pdf/building_list_chi.pdf');
+  await downloadPdf(`data/${today}`, 'building_list_eng.pdf', 'https://www.chp.gov.hk/files/pdf/building_list_eng.pdf');
+  await downloadPdf(`data/${today}`, 'flights_trains_tc.pdf', 'https://www.chp.gov.hk/files/pdf/flights_trains_tc.pdf');
+  await downloadPdf(`data/${today}`, 'flights_trains_en.pdf', 'https://www.chp.gov.hk/files/pdf/flights_trains_en.pdf');
+  generateHtml();
+}
+
+// don't cache. throw the error to let travis know
+run();
---
+++ scripts/git_update.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+# Credit: https://gist.github.com/willprice/e07efd73fb7f13f917ea
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.com"
+  git config --global user.name "Travis CI"
+}
+
+commit_atm_files() {
+  git checkout master
+
+  # Move the data files
+  if [ -f transformer/checksum/hang_seng.md5 ]; then
+    cp -f transformer/processed/hang_seng.json web/src/data
+  fi
+
+  if [ -f transformer/checksum/hsbc.md5 ]; then
+    cp -f transformer/processed/hsbc.json web/src/data
+  fi
+
+  if [ -f transformer/checksum/jetco_en.md5 ]; then
+    cp -f transformer/processed/jetco_en.json web/src/data
+  fi
+
+  if [ -f transformer/checksum/jetco_tc.md5 ]; then
+    cp -f transformer/processed/jetco_tc.json web/src/data
+  fi
+
+  git status
+  # Current month and year, e.g: Apr 2018
+  dateAndMonth=`date "+%b %Y"`
+  # Stage the modified files in dist/output
+  git add -f web/src/data/
+  # Create a new commit with a custom build message
+  # with "[skip ci]" to avoid a build loop
+  # and Travis build number for reference
+  git commit -m "Travis update processed json: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+  # Update Checksum files
+  git add -f transformer/checksum/
+  git commit -m "Travis update checksum files: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+}
+
+upload_files() {
+  git remote add upstream https://nandiheath:${GH_TOKEN}@github.com/nandiheath/chp-archive.git > /dev/null 2>&1
+  git push --quiet --set-upstream upstream master
+}
+
+setup_git
+
+commit_atm_files
+
+# Attempt to commit to git only if "git commit" succeeded
+if [ $? -eq 0 ]; then
+  echo "A new commit with changed ATM JSON files exists. Uploading to GitHub"
+  upload_files
+else
+  echo "No changes in ATM JSON files. Nothing to do"
+fi
